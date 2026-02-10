dcea5e44f9b85e4a211afda558224ae687fc252d
ThomasHatesIT <thomasbernardo910@gmail.com>
2026-02-11 01:48:06 +0800

    submitquiz function

--- backend/controllers/quizController.js
+++ backend/controllers/quizController.js
@@ -59,6 +59,79 @@ import Quiz from '../models/Quiz.js'
  }
  export const submitQuiz = async (req, res, next) => {
     try{
+        const {answers} = req.body
+        if(!Array.isArray(answers)){
+            return res.status(400).json({
+                success:false,
+                message:'Quiz not found',
+                statusCode: 400 
+            })
+        }
+
+        const quiz = await Quiz.findOne({
+             _id: req.params.id,
+             userId: req.user._id
+        });
+        if(!quiz){
+            return res.status(404).json({
+                success:false,
+                message:'Quiz not found',
+                statusCode: 404 
+            })
+        }
+
+        if(quiz.completedAt){
+             return res.status(400).json({
+                success:false,
+                message:'Quiz Already completed',
+                statusCode: 400 
+            })
+        }
+
+        //process asnwers
+        let correctCount = 0
+        const userAnswers = []
+
+        answers.forEach(answer => {
+            const {questionIndex, selectedAnswer } = answer
+
+            if(questionIndex < quiz.questions.length) {
+                const question = quiz.questions[questionIndex]
+                const isCorrect = selectedAnswer === question.correctedAnswer
+            }
+
+            if (isCorrect) correctCount++
+
+            userAnswers.push({
+                questionIndex,
+                selectedAnswer,
+                isCorrect,
+                answeredAt: new Date(),
+            })
+        })
+        
+        //calculate score
+        const score = Math.rount((correctCount / quiz.totalQustions)* 100)
+
+        //update Quiz
+        quiz.userAnswers = userAnswers
+        quiz.score = score
+        quiz.completedAt = new Date()
+
+        await quiz.save()
+
+          res.status(200).json({
+            success:true,
+            message:'Quiz answers submitted successfully',
+            data: {
+                quizId: quiz._id,
+                score,
+                correctCount,
+                totalQuestions: quiz.totalQuestions,
+                percentage: score,
+                userAnswers
+            }
+        })
 
     }catch(err){
         next(err)
