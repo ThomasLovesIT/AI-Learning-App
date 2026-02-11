9c17173e64b3e63cf49e9a2f7ee300253fb2f5f0
ThomasHatesIT <thomasbernardo910@gmail.com>
2026-02-11 18:15:44 +0800

    quiz results

--- backend/controllers/quizController.js
+++ backend/controllers/quizController.js
@@ -4,7 +4,7 @@ import Quiz from '../models/Quiz.js'
     try{
         const quizzes =  await Quiz.find({
          userId: req.user._id,
-           documentId: req.params.documentId
+        documentId: req.params.documentId
         })
         .populate('documentId','title')
         .sort({ createdAt: -1 })
@@ -152,7 +152,53 @@ import Quiz from '../models/Quiz.js'
 
  export const getQuizResults = async (req, res, next) => {
     try{
-         
+         const quiz = await Quiz.findOne({
+          _id: req.params._id,
+          userId: req.user._id
+         }).populate('documentId', 'title');
+         if(!quiz){
+          return res.status(404).json({
+             success:false,
+                message:'Quiz not found',
+                statusCode: 404
+          })
+         }
+         // find the quiz where the "completedAt: true"
+         if(!quiz.completedAt){
+          return res.status(400).json({
+             success:false,
+                message:'Quiz not complete',
+                statusCode: 400
+          })
+         }
+         //display the score and total questions
+         const detailedResults =  quiz.questions.map((question,index) => {
+          const userAnswer = quiz.userAnswers.find(a => a.questionIndex === index)
+
+          return {
+            questionIndex: index,
+            question: question.question,
+            options: question.options,
+            correctAnswer: question.correctAnswer,
+            selectedAnswer: userAnswer?.selectedAnswer || null,
+            isCorrect: userAnswer?.isCorrect || false,
+            explanation: question.explanation
+         }
+         })
+
+          res.status(200).json({
+        success: true,
+        message: 'Quiz Results success',
+        quizDetails: {
+          id: quiz._id,
+          title: quiz.title,
+          document: quiz.documentId,
+          score: quiz.score,
+          totalQuestions: quiz.totalQuestions,
+          completedAt: quiz.completedAt
+        },
+        results: detailedResults   
+    });
     }catch(err){
         next(err)
     }
